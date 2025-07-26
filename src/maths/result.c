d3c647256845a6d892f6f4e05ac88e258920ecb1
Joe Watkins <krakjoe@php.net>
2025-07-26 20:39:58 +0200

    drop this

--- src/maths/result.c
+++ src/maths/result.c
@@ -478,13 +478,7 @@ ort_tensor_t* ort_math_result_tensor(
     tensor->data = ort_alloc(
         php_ort_tensor_sizeof(tensor),
         tensor->elements);
-
-    if (tensor->type == ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL) {
-        memset(tensor->data,
-            php_ort_tensor_sizeof(tensor),
-            tensor->elements);
-    }
-
+    
     /* Generate name */
     char name_buffer[64];
     snprintf(name_buffer,
